26002c1721d56a23ee7df9bf4a0b0e3a7e7ba77d
Arturo Suárez <manuel.suarez@cimat.mx>
2022-09-26 16:52:21 -0500

    Datapaths

--- main.py
+++ main.py
@@ -17,3 +17,27 @@ from keras.utils.vis_utils import plot_model
 AUTOTUNE = tf.data.AUTOTUNE
 
 print(tf.__version__)
+
+# Variables de configuración
+BUFFER_SIZE = 1000
+BATCH_SIZE = 1
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
+
+INPUT_DIM     = (256,256,3)
+OUTPUT_CHANNELS = 1
+BATCH_SIZE    = 10
+R_LOSS_FACTOR = 10000
+EPOCHS        = 100
+INITIAL_EPOCH = 0
+
+# Configuración de rutas para la carga de conjuntos de datos
+local_path = '/home/est_posgrado_manuel.suarez/data/dogs-vs-cats/train'
+train_dogs_files = glob(os.path.join(local_path, 'dog.*.jpg'))
+train_cats_files = glob(os.path.join(local_path, 'cat.*.jpg'))
+train_dogs_files.sort()
+train_cats_files.sort()
+train_dogs_files = np.array(train_dogs_files)
+train_cats_files = np.array(train_cats_files)
+
+print(len(train_dogs_files), len(train_cats_files))
