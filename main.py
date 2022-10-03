723a909021fe44c110fa6cd4f9dc203fd413e367
Arturo Suárez <manuel.suarez@cimat.mx>
2022-10-03 11:23:19 -0500

    Incorpored VAE model

--- main.py
+++ main.py
@@ -119,6 +119,281 @@ plt.savefig('figure_2.png')
 # Configure Pix2Pix model
 OUTPUT_CHANNELS = 3
 
+# VAE components
+class Encoder(keras.Model):
+    def __init__(self, input_dim, output_dim, encoder_conv_filters, encoder_conv_kernel_size, encoder_conv_strides,
+                 use_batch_norm=True, use_dropout=True, **kwargs):
+        '''
+        '''
+        super(Encoder, self).__init__(**kwargs)
+
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.encoder_conv_filters = encoder_conv_filters
+        self.encoder_conv_kernel_size = encoder_conv_kernel_size
+        self.encoder_conv_strides = encoder_conv_strides
+        self.n_layers_encoder = len(self.encoder_conv_filters)
+        self.use_batch_norm = use_batch_norm
+        self.use_dropout = use_dropout
+
+        self.model = self.encoder_model()
+        self.built = True
+
+    def get_config(self):
+        config = super(Encoder, self).get_config()
+        config.update({"units": self.units})
+        return config
+
+    def encoder_model(self):
+        '''
+        '''
+        encoder_input = keras.layers.Input(shape=self.input_dim, name='encoder')
+        x = encoder_input
+
+        for i in range(self.n_layers_encoder):
+            x = keras.layers.Conv2D(filters=self.encoder_conv_filters[i],
+                       kernel_size=self.encoder_conv_kernel_size[i],
+                       strides=self.encoder_conv_strides[i],
+                       padding='same',
+                       name='encoder_conv_' + str(i), )(x)
+            if self.use_batch_norm:
+                x = keras.layers.BatchNormalization()(x)
+            x = keras.layers.LeakyReLU()(x)
+            if self.use_dropout:
+                x = keras.layers.Dropout(rate=0.25)(x)
+
+        self.last_conv_size = x.shape[1:]
+        x = keras.layers.Flatten()(x)
+        encoder_output = keras.layers.Dense(self.output_dim)(x)
+        model = keras.Model(encoder_input, encoder_output)
+        return model
+
+    def call(self, inputs):
+        '''
+        '''
+        return self.model(inputs)
+
+
+class Decoder(keras.Model):
+    def __init__(self, input_dim, input_conv_dim,
+                 decoder_conv_t_filters, decoder_conv_t_kernel_size, decoder_conv_t_strides,
+                 use_batch_norm=True, use_dropout=True, **kwargs):
+
+        '''
+        '''
+        super(Decoder, self).__init__(**kwargs)
+
+        self.input_dim = input_dim
+        self.input_conv_dim = input_conv_dim
+
+        self.decoder_conv_t_filters = decoder_conv_t_filters
+        self.decoder_conv_t_kernel_size = decoder_conv_t_kernel_size
+        self.decoder_conv_t_strides = decoder_conv_t_strides
+        self.n_layers_decoder = len(self.decoder_conv_t_filters)
+
+        self.use_batch_norm = use_batch_norm
+        self.use_dropout = use_dropout
+
+        self.model = self.decoder_model()
+        self.built = True
+
+    def get_config(self):
+        config = super(Decoder, self).get_config()
+        config.update({"units": self.units})
+        return config
+
+    def decoder_model(self):
+        '''
+        '''
+        decoder_input = keras.layers.Input(shape=self.input_dim, name='decoder')
+        x = keras.layers.Dense(np.prod(self.input_conv_dim))(decoder_input)
+        x = keras.layers.Reshape(self.input_conv_dim)(x)
+
+        for i in range(self.n_layers_decoder):
+            x = keras.layers.Conv2DTranspose(filters=self.decoder_conv_t_filters[i],
+                                kernel_size=self.decoder_conv_t_kernel_size[i],
+                                strides=self.decoder_conv_t_strides[i],
+                                padding='same',
+                                name='decoder_conv_t_' + str(i))(x)
+            if i < self.n_layers_decoder - 1:
+
+                if self.use_batch_norm:
+                    x = keras.layers.BatchNormalization()(x)
+                x = keras.layers.LeakyReLU()(x)
+                if self.use_dropout:
+                    x = keras.layers.Dropout(rate=0.25)(x)
+            else:
+                x = keras.layers.Activation('sigmoid')(x)
+        decoder_output = x
+        model = keras.Model(decoder_input, decoder_output)
+        return model
+
+    def call(self, inputs):
+        '''
+        '''
+        return self.model(inputs)
+
+
+class Sampler(keras.Model):
+    """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
+
+    def __init__(self, latent_dim, **kwargs):
+        super(Sampler, self).__init__(**kwargs)
+        self.latent_dim = latent_dim
+        self.model = self.sampler_model()
+        self.built = True
+
+    def get_config(self):
+        config = super(Sampler, self).get_config()
+        config.update({"units": self.units})
+        return config
+
+    def sampler_model(self):
+        '''
+        input_dim is a vector in the latent (codified) space
+        '''
+        input_data = keras.layers.Input(shape=self.latent_dim)
+        z_mean = keras.layers.Dense(self.latent_dim, name="z_mean")(input_data)
+        z_log_var = keras.layers.Dense(self.latent_dim, name="z_log_var")(input_data)
+
+        self.batch = tf.shape(z_mean)[0]
+        self.dim = tf.shape(z_mean)[1]
+
+        epsilon = tf.keras.backend.random_normal(shape=(self.batch, self.dim))
+        z = z_mean + tf.exp(0.5 * z_log_var) * epsilon
+        model = keras.Model(input_data, [z, z_mean, z_log_var])
+        return model
+
+    def call(self, inputs):
+        '''
+        '''
+        return self.model(inputs)
+
+class VAE(keras.Model):
+    def __init__(self, r_loss_factor=1, summary=False, **kwargs):
+        super(VAE, self).__init__(**kwargs)
+
+        self.r_loss_factor = r_loss_factor
+
+        # Architecture
+        self.input_dim = INPUT_DIM
+        self.latent_dim = LATENT_DIM
+        self.encoder_conv_filters = [64, 64, 64, 64]
+        self.encoder_conv_kernel_size = [3, 3, 3, 3]
+        self.encoder_conv_strides = [2, 2, 2, 2]
+        self.n_layers_encoder = len(self.encoder_conv_filters)
+
+        self.decoder_conv_t_filters = [64, 64, 64, 3]
+        self.decoder_conv_t_kernel_size = [3, 3, 3, 3]
+        self.decoder_conv_t_strides = [2, 2, 2, 2]
+        self.n_layers_decoder = len(self.decoder_conv_t_filters)
+
+        self.use_batch_norm = True
+        self.use_dropout = True
+
+        self.total_loss_tracker = tf.keras.metrics.Mean(name="total_loss")
+        self.reconstruction_loss_tracker = tf.keras.metrics.Mean(name="reconstruction_loss")
+        self.kl_loss_tracker = tf.keras.metrics.Mean(name="kl_loss")
+        self.mae = tf.keras.losses.MeanAbsoluteError()
+
+        # Encoder
+        self.encoder_model = Encoder(input_dim=self.input_dim,
+                                     output_dim=self.latent_dim,
+                                     encoder_conv_filters=self.encoder_conv_filters,
+                                     encoder_conv_kernel_size=self.encoder_conv_kernel_size,
+                                     encoder_conv_strides=self.encoder_conv_strides,
+                                     use_batch_norm=self.use_batch_norm,
+                                     use_dropout=self.use_dropout)
+        self.encoder_conv_size = self.encoder_model.last_conv_size
+        if summary:
+            self.encoder_model.summary()
+
+        # Sampler
+        self.sampler_model = Sampler(latent_dim=self.latent_dim)
+        if summary:
+            self.sampler_model.summary()
+
+        # Decoder
+        self.decoder_model = Decoder(input_dim=self.latent_dim,
+                                     input_conv_dim=self.encoder_conv_size,
+                                     decoder_conv_t_filters=self.decoder_conv_t_filters,
+                                     decoder_conv_t_kernel_size=self.decoder_conv_t_kernel_size,
+                                     decoder_conv_t_strides=self.decoder_conv_t_strides,
+                                     use_batch_norm=self.use_batch_norm,
+                                     use_dropout=self.use_dropout)
+        if summary: self.decoder_model.summary()
+
+        self.built = True
+
+    @property
+    def metrics(self):
+        return [self.total_loss_tracker,
+                self.reconstruction_loss_tracker,
+                self.kl_loss_tracker, ]
+
+    @tf.function
+    def train_step(self, data):
+        dog_img = data[0]
+        cat_img = data[1]
+        with tf.GradientTape() as tape:
+            # predict
+            x = self.encoder_model(dog_img)
+            z, z_mean, z_log_var = self.sampler_model(x)
+            pred = self.decoder_model(z)
+
+            # loss
+            r_loss = self.r_loss_factor * self.mae(cat_img, pred)
+            kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
+            total_loss = r_loss + kl_loss
+
+        # gradient
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        # train step
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+
+        # compute progress
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(r_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        return {"loss": self.total_loss_tracker.result(),
+                "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+                "kl_loss": self.kl_loss_tracker.result(), }
+
+    @tf.function
+    def generate(self, z_sample):
+        '''
+        We use the sample of the N(0,I) directly as
+        input of the deterministic generator.
+        '''
+        return self.decoder_model(z_sample)
+
+    @tf.function
+    def codify(self, images):
+        '''
+        For an input image we obtain its particular distribution:
+        its mean, its variance (unvertaintly) and a sample z of such distribution.
+        '''
+        x = self.encoder_model.predict(images)
+        z, z_mean, z_log_var = self.sampler_model(x)
+        return z, z_mean, z_log_var
+
+    # implement the call method
+    @tf.function
+    def call(self, inputs, training=False):
+        '''
+        '''
+        tmp1, tmp2 = self.encoder_model.use_Dropout, self.decoder_model.use_Dropout
+        if not training:
+            self.encoder_model.use_Dropout, self.decoder_model.use_Dropout = False, False
+
+        x = self.encoder_model(inputs)
+        z, z_mean, z_log_var = self.sampler_model(x)
+        pred = self.decoder_model(z)
+
+        self.encoder_model.use_Dropout, self.decoder_model.use_Dropout = tmp1, tmp2
+        return pred
+
 # Loss function
 # Loss Functions
 def discriminator_loss(loss_obj, real, generated):
@@ -138,14 +413,31 @@ def identity_loss(real_image, same_image):
     loss = tf.reduce_mean(tf.abs(real_image - same_image))
     return LAMBDA * 0.5 * loss
 
+INPUT_DIM     = (IMG_WIDTH,IMG_HEIGHT,3)
+LATENT_DIM    = 150
 LAMBDA = 10
+R_LOSS_FACTOR = 100000
 
 class CycleGAN(keras.Model):
-    def __init__(self, p_lambda=LAMBDA, summary=False, **kwargs):
+    def __init__(self, p_lambda=LAMBDA, r_loss_factor=R_LOSS_FACTOR, **kwargs):
         super(CycleGAN, self).__init__(**kwargs)
         self.p_lambda = p_lambda
 
-        # Architecture
+        # VAE Model
+        self.vae = VAE(r_loss_factor=r_loss_factor, summary=False)
+
+        # VAE Optimizer
+        self.vae_optimizer = keras.optimizers.Adam()
+
+        # VAE Loss
+        self.mae = tf.keras.losses.MeanAbsoluteError()
+
+        # VAE Metrics
+        self.total_loss_tracker = tf.keras.metrics.Mean(name="total_loss")
+        self.reconstruction_loss_tracker = tf.keras.metrics.Mean(name="reconstruction_loss")
+        self.kl_loss_tracker = tf.keras.metrics.Mean(name="kl_loss")
+
+        # Cycle-GAN Architecture
         self.generator_g = pix2pix.unet_generator(OUTPUT_CHANNELS, norm_type='instancenorm')
         self.generator_f = pix2pix.unet_generator(OUTPUT_CHANNELS, norm_type='instancenorm')
 
@@ -162,22 +454,37 @@ class CycleGAN(keras.Model):
         # Loss
         self.loss_obj = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
-        # Metric trackers
+        # Cycle-GAN Metrics
         self.total_cycle_loss_tracker = tf.keras.metrics.Mean(name="total_cycle_loss")
         self.total_gen_g_loss_tracker = tf.keras.metrics.Mean(name="total_gen_g_loss")
         self.total_gen_f_loss_tracker = tf.keras.metrics.Mean(name="total_gen_f_loss")
         self.disc_x_loss_tracker = tf.keras.metrics.Mean(name="disc_x_loss")
         self.disc_y_loss_tracker = tf.keras.metrics.Mean(name="disc_y_loss")
 
+        self.built = True
+
     @tf.function
     def train_step(self, data):
         # persistent is set to True because the tape is used more than
         # once to calculate the gradients.
+        # real_x: dog image
+        # real_y: cat image
         real_x, real_y = data
         with tf.GradientTape(persistent=True) as tape:
+            # VAE Operations
+            vae_x = self.vae.encoder_model(real_x)
+            vae_z, z_mean, z_log_var = self.vae.sampler_model(vae_x)
+            vae_y = self.vae.decoder_model(vae_z)
+
+            # VAE Loss
+            r_loss = self.vae.r_loss_factor * self.vae.mae(real_y, vae_y)
+            kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
+            total_loss = r_loss + kl_loss
+
+            # Cycle-GAN Operations
             # Generator G translates X -> Y
             # Generator F translates Y -> X.
-
             fake_y = self.generator_g(real_x, training=True)
             cycled_x = self.generator_f(fake_y, training=True)
 
@@ -207,6 +514,13 @@ class CycleGAN(keras.Model):
             disc_x_loss = discriminator_loss(self.loss_obj, disc_real_x, disc_fake_x)
             disc_y_loss = discriminator_loss(self.loss_obj, disc_real_y, disc_fake_y)
 
+        # VAE Gradients
+        vae_gradients = tape.gradient(total_loss, self.vae.trainable_weights)
+
+        # VAE optimizer step
+        self.vae_optimizer.apply_gradients(zip(vae_gradients,
+                                               self.vae.trainable_weights))
+
         # Calculate the gradients for generator and discriminator
         generator_g_gradients = tape.gradient(total_gen_g_loss,
                                               self.generator_g.trainable_variables)
@@ -232,12 +546,20 @@ class CycleGAN(keras.Model):
                                                       self.discriminator_y.trainable_variables))
 
         # compute progress
+        # vae
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(r_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        # cycle-gan
         self.total_cycle_loss_tracker.update_state(total_cycle_loss)
         self.total_gen_g_loss_tracker.update_state(total_gen_g_loss)
         self.total_gen_f_loss_tracker.update_state(total_gen_f_loss)
         self.disc_x_loss_tracker.update_state(disc_x_loss)
         self.disc_y_loss_tracker.update_state(disc_y_loss)
         return {
+            "vae_loss": self.total_loss_tracker.result(),
+            "vae_reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "vae_kl_loss": self.kl_loss_tracker.result(),
             "total_cycle_loss": self.total_cycle_loss_tracker.result(),
             "total_gen_g_loss": self.total_gen_g_loss_tracker.result(),
             "total_gen_f_loss": self.total_gen_f_loss_tracker.result(),
