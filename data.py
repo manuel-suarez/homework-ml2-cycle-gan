8ca3ed1d4d8e5c4118be3c6c36bef34584ee263e
Arturo Suárez <manuel.suarez@cimat.mx>
2022-10-04 17:02:43 -0500

    Put data loading in separate script

---
+++ data.py
@@ -0,0 +1,141 @@
+import os
+import numpy as np
+import tensorflow as tf
+from glob import glob
+from matplotlib import pyplot as plt
+
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
+
+AUTOTUNE = tf.data.AUTOTUNE
+def read_and_decode(file):
+    img = tf.io.read_file(file)
+    img = tf.image.decode_jpeg(img)
+    img = tf.cast(img, tf.float32)
+    # img = img / 255.0
+    # img = tf.image.resize(img, INPUT_DIM[:2], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    return img
+
+def load_image(file1):
+    return read_and_decode(file1)
+
+def random_crop(image):
+  cropped_image = tf.image.random_crop(
+      image, size=[IMG_HEIGHT, IMG_WIDTH, 3])
+
+  return cropped_image
+
+# normalizing the images to [-1, 1]
+def normalize(image):
+  image = tf.cast(image, tf.float32)
+  image = (image / 127.5) - 1
+  return image
+
+def random_jitter(image):
+  # resizing to 286 x 286 x 3
+  image = tf.image.resize(image, [286, 286],
+                          method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+
+  # randomly cropping to 256 x 256 x 3
+  image = random_crop(image)
+
+  # random mirroring
+  image = tf.image.random_flip_left_right(image)
+
+  return image
+
+def preprocess_image_train(image):
+  image = random_jitter(image)
+  image = normalize(image)
+  return image
+
+def preprocess_image_test(image):
+  image = normalize(image)
+  return image
+
+# Dataset's configuration
+# train_dataset = tf.data.Dataset.zip((dog_dataset, cat_dataset))
+# train_dataset = train_dataset.shuffle(buffer_size=n_images, reshuffle_each_iteration=True)
+# train_dataset = train_dataset.map(load_image, num_parallel_calls=tf.data.AUTOTUNE)
+# train_dataset = train_dataset.batch(BATCH_SIZE).repeat()
+
+# Example configuration
+# train_dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels)).shuffle(BUFFER_SIZE).batch(GLOBAL_BATCH_SIZE)
+# test_dataset = tf.data.Dataset.from_tensor_slices((test_images, test_labels)).batch(GLOBAL_BATCH_SIZE)
+
+def build_data(data_folder, global_batch_size):
+    # Input pipeline
+    dog_files = np.array(glob(os.path.join(data_folder, 'dog.*.jpg')))
+    cat_files = np.array(glob(os.path.join(data_folder, 'cat.*.jpg')))
+
+    BUFFER_SIZE = len(dog_files)
+
+    train_dogs = tf.data.Dataset.list_files(dog_files, shuffle=False)
+    train_dogs = train_dogs.map(load_image, num_parallel_calls=tf.data.AUTOTUNE)
+    train_dogs = train_dogs.cache().map(
+        preprocess_image_train, num_parallel_calls=AUTOTUNE).shuffle(
+        BUFFER_SIZE).batch(global_batch_size)
+
+    train_cats = tf.data.Dataset.list_files(cat_files, shuffle=False)
+    train_cats = train_cats.map(load_image, num_parallel_calls=tf.data.AUTOTUNE)
+    train_cats = train_cats.cache().map(
+        preprocess_image_train, num_parallel_calls=AUTOTUNE).shuffle(
+        BUFFER_SIZE).batch(global_batch_size)
+
+    return train_dogs, train_cats, BUFFER_SIZE
+
+def generate_figure_1_2(train_dogs, train_cats):
+    sample_dog = next(iter(train_dogs))
+    sample_cat = next(iter(train_cats))
+
+    plt.subplot(121)
+    plt.title('Dog')
+    plt.imshow(sample_dog[0] * 0.5 + 0.5)
+
+    plt.subplot(122)
+    plt.title('Dog with random jitter')
+    plt.imshow(random_jitter(sample_dog[0]) * 0.5 + 0.5)
+
+    print("Plotting dog")
+    plt.savefig('figure_1.png')
+
+    plt.subplot(121)
+    plt.title('Cat')
+    plt.imshow(sample_cat[0] * 0.5 + 0.5)
+
+    plt.subplot(122)
+    plt.title('Cat with random jitter')
+    plt.imshow(random_jitter(sample_cat[0]) * 0.5 + 0.5)
+
+    print("Plotting cat")
+    plt.savefig('figure_2.png')
+
+def generate_sample(generator_g, generator_f, discriminator_x, discriminator_y):
+    to_cat = generator_g(sample_dog)
+    to_dog = generator_f(sample_cat)
+    plt.figure(figsize=(8, 8))
+    contrast = 8
+
+    imgs = [sample_dog, to_cat, sample_cat, to_dog]
+    title = ['Dog', 'To Cat', 'Cat', 'To Dog']
+
+    for i in range(len(imgs)):
+        plt.subplot(2, 2, i + 1)
+        plt.title(title[i])
+        if i % 2 == 0:
+            plt.imshow(imgs[i][0] * 0.5 + 0.5)
+        else:
+            plt.imshow(imgs[i][0] * 0.5 * contrast + 0.5)
+    plt.savefig('figure_3.png')
+
+    plt.figure(figsize=(8, 8))
+
+    plt.subplot(121)
+    plt.title('Is a real cat?')
+    plt.imshow(discriminator_y(sample_cat)[0, ..., -1], cmap='RdBu_r')
+
+    plt.subplot(122)
+    plt.title('Is a real dog?')
+    plt.imshow(discriminator_x(sample_dog)[0, ..., -1], cmap='RdBu_r')
+
+    plt.savefig('figure_4.png')
--- main.py
+++ main.py
@@ -16,20 +16,20 @@ import time
 import matplotlib.pyplot as plt
 
 from models import VAE
-
-AUTOTUNE = tf.data.AUTOTUNE
+from data import build_data, generate_figure_1_2, generate_sample
 
 # Execution strategy
 mirrored_strategy = tf.distribute.MirroredStrategy()
 print('Number of devices for distributed strategy: {}'.format(mirrored_strategy.num_replicas_in_sync))
 # Check GPU execution
 
-# Input pipeline
-DATA_FOLDER   = '/home/est_posgrado_manuel.suarez/data/dogs-vs-cats/train'
-dog_files = np.array(glob(os.path.join(DATA_FOLDER, 'dog.*.jpg')))
-cat_files = np.array(glob(os.path.join(DATA_FOLDER, 'cat.*.jpg')))
+BATCH_SIZE_PER_REPLICA = 64
+GLOBAL_BATCH_SIZE = BATCH_SIZE_PER_REPLICA * mirrored_strategy.num_replicas_in_sync
 
-BUFFER_SIZE = len(dog_files)
+DATA_FOLDER = '/home/est_posgrado_manuel.suarez/data/dogs-vs-cats/train'
+train_dogs, train_cats, BUFFER_SIZE = build_data(DATA_FOLDER, global_batch_size=GLOBAL_BATCH_SIZE)
+generate_figure_1_2(train_dogs, train_cats)
+
 IMG_WIDTH = 256
 IMG_HEIGHT = 256
 OUTPUT_DIM = 3
@@ -39,103 +39,12 @@ LATENT_DIM = 150
 LAMBDA = 10
 R_LOSS_FACTOR = 10000
 
-BATCH_SIZE_PER_REPLICA = 64
-GLOBAL_BATCH_SIZE = BATCH_SIZE_PER_REPLICA * mirrored_strategy.num_replicas_in_sync
 
 EPOCHS = 10
 
-n_images        = dog_files.shape[0]
-steps_per_epoch = n_images//BATCH_SIZE_PER_REPLICA
-print('num image files : ', n_images)
-print('steps per epoch : ', steps_per_epoch )
-
-def read_and_decode(file):
-    img = tf.io.read_file(file)
-    img = tf.image.decode_jpeg(img)
-    img = tf.cast(img, tf.float32)
-    # img = img / 255.0
-    # img = tf.image.resize(img, INPUT_DIM[:2], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-    return img
-
-def load_image(file1):
-    return read_and_decode(file1)
-
-def random_crop(image):
-  cropped_image = tf.image.random_crop(
-      image, size=[IMG_HEIGHT, IMG_WIDTH, 3])
-
-  return cropped_image
-
-# normalizing the images to [-1, 1]
-def normalize(image):
-  image = tf.cast(image, tf.float32)
-  image = (image / 127.5) - 1
-  return image
-
-def random_jitter(image):
-  # resizing to 286 x 286 x 3
-  image = tf.image.resize(image, [286, 286],
-                          method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-
-  # randomly cropping to 256 x 256 x 3
-  image = random_crop(image)
-
-  # random mirroring
-  image = tf.image.random_flip_left_right(image)
-
-  return image
-
-def preprocess_image_train(image):
-  image = random_jitter(image)
-  image = normalize(image)
-  return image
-
-def preprocess_image_test(image):
-  image = normalize(image)
-  return image
-
-# Dataset's configuration
-# train_dataset = tf.data.Dataset.zip((dog_dataset, cat_dataset))
-# train_dataset = train_dataset.shuffle(buffer_size=n_images, reshuffle_each_iteration=True)
-# train_dataset = train_dataset.map(load_image, num_parallel_calls=tf.data.AUTOTUNE)
-# train_dataset = train_dataset.batch(BATCH_SIZE).repeat()
-
-train_dogs = tf.data.Dataset.list_files(dog_files, shuffle=False)
-train_dogs = train_dogs.map(load_image, num_parallel_calls=tf.data.AUTOTUNE)
-train_dogs = train_dogs.cache().map(
-    preprocess_image_train, num_parallel_calls=AUTOTUNE).shuffle(
-    BUFFER_SIZE).batch(GLOBAL_BATCH_SIZE)
-
-train_cats = tf.data.Dataset.list_files(cat_files, shuffle=False)
-train_cats = train_cats.map(load_image, num_parallel_calls=tf.data.AUTOTUNE)
-train_cats = train_cats.cache().map(
-    preprocess_image_train, num_parallel_calls=AUTOTUNE).shuffle(
-    BUFFER_SIZE).batch(GLOBAL_BATCH_SIZE)
-
-sample_dog = next(iter(train_dogs))
-sample_cat = next(iter(train_cats))
-
-plt.subplot(121)
-plt.title('Dog')
-plt.imshow(sample_dog[0] * 0.5 + 0.5)
-
-plt.subplot(122)
-plt.title('Dog with random jitter')
-plt.imshow(random_jitter(sample_dog[0]) * 0.5 + 0.5)
-
-print("Plotting dog")
-plt.savefig('figure_1.png')
-
-plt.subplot(121)
-plt.title('Cat')
-plt.imshow(sample_cat[0] * 0.5 + 0.5)
-
-plt.subplot(122)
-plt.title('Cat with random jitter')
-plt.imshow(random_jitter(sample_cat[0]) * 0.5 + 0.5)
-
-print("Plotting cat")
-plt.savefig('figure_2.png')
+steps_per_epoch = BUFFER_SIZE//BATCH_SIZE_PER_REPLICA
+print('num image files : ', BUFFER_SIZE)
+print('steps per epoch : ', steps_per_epoch)
 
 # Configure Pix2Pix model
 OUTPUT_CHANNELS = 3
@@ -350,34 +259,7 @@ def distributed_train_step(dist_inputs):
     mirrored_strategy.run(train_step, args=(dist_inputs,))
     # return mirrored_strategy.reduce(tf.distribute.ReduceOp.SUM, per_replica_losses, axis=None)
 
-to_cat = generator_g(sample_dog)
-to_dog = generator_f(sample_cat)
-plt.figure(figsize=(8, 8))
-contrast = 8
-
-imgs = [sample_dog, to_cat, sample_cat, to_dog]
-title = ['Dog', 'To Cat', 'Cat', 'To Dog']
-
-for i in range(len(imgs)):
-  plt.subplot(2, 2, i+1)
-  plt.title(title[i])
-  if i % 2 == 0:
-    plt.imshow(imgs[i][0] * 0.5 + 0.5)
-  else:
-    plt.imshow(imgs[i][0] * 0.5 * contrast + 0.5)
-plt.savefig('figure_3.png')
-
-plt.figure(figsize=(8, 8))
-
-plt.subplot(121)
-plt.title('Is a real cat?')
-plt.imshow(discriminator_y(sample_cat)[0, ..., -1], cmap='RdBu_r')
-
-plt.subplot(122)
-plt.title('Is a real dog?')
-plt.imshow(discriminator_x(sample_dog)[0, ..., -1], cmap='RdBu_r')
-
-plt.savefig('figure_4.png')
+generate_sample(generator_g, generator_f, discriminator_x, discriminator_y)
 print("Model builded")
 
 # Checkpoints
