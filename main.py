3000135d125212f54e54d8867134e3b702ab9776
Arturo Suárez <manuel.suarez@cimat.mx>
2022-10-05 19:53:25 -0500

    Increasing latent dim

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ mirrored_strategy = tf.distribute.get_strategy()
 info('Number of devices for distributed strategy: {}'.format(mirrored_strategy.num_replicas_in_sync))
 # Check GPU execution
 
-BATCH_SIZE_PER_REPLICA = 1
+BATCH_SIZE_PER_REPLICA = 24
 GLOBAL_BATCH_SIZE = BATCH_SIZE_PER_REPLICA * mirrored_strategy.num_replicas_in_sync
 
 DATA_FOLDER = '/home/est_posgrado_manuel.suarez/data/dogs-vs-cats/train5000'
@@ -40,7 +40,7 @@ IMG_HEIGHT = 256
 OUTPUT_DIM = 3
 
 INPUT_DIM = (IMG_WIDTH, IMG_HEIGHT, OUTPUT_DIM)
-LATENT_DIM = 150
+LATENT_DIM = 600
 LAMBDA = 10
 R_LOSS_FACTOR = 10000
 
